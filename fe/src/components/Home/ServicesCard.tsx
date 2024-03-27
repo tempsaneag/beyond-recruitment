c92b873be1abebc3deae89b6e303bb80906cfe01
saneag <garstea2002@mail.ru>
2024-03-27 19:03:44 +0200

    added hover effect on service card

--- fe/src/components/Home/ServicesCard.tsx
+++ fe/src/components/Home/ServicesCard.tsx
@@ -5,7 +5,9 @@ import {
   CardHeader,
 } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
+import { usePageTransitionStore } from '@/store/pageTransitionStore';
 import { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface ServicesCardProps {
   icon: ReactNode;
@@ -20,14 +22,27 @@ export default function ServicesCard({
   description,
   footer,
 }: ServicesCardProps) {
+  const navigate = useNavigate();
+  const setSwipeDirection = usePageTransitionStore(
+    (state) => state.setSwipeDirection
+  );
+
+  const handleCardClick = () => {
+    setSwipeDirection('left');
+    navigate('/services');
+  };
+
   return (
-    <Card className='max-w-md transition-all duration-300 hover:shadow-lg'>
+    <Card
+      className='group max-w-md cursor-pointer bg-gray-200 transition-all duration-300 hover:bg-gray-700 hover:text-white hover:shadow-lg'
+      onClick={handleCardClick}
+    >
       <CardHeader>
         <div className='flex flex-col items-center gap-2'>
           {icon}
           <span className='text-lg'>{title}</span>
         </div>
-        <Separator />
+        <Separator className='bg-gray-400 transition-all duration-300 group-hover:bg-white' />
       </CardHeader>
       <CardContent>
         <p className='text-center'>{description}</p>
