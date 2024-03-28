0be214c5f42126303f339313bb1defc43a38cd42
saneag <garstea2002@mail.ru>
2024-03-28 12:10:34 +0200

    updated home logo and text

--- fe/src/components/Home/MainSection.tsx
+++ fe/src/components/Home/MainSection.tsx
@@ -1,37 +1,24 @@
+import { motion } from 'framer-motion';
+
 export default function MainSection() {
   return (
-    <div
-      className="flex size-full min-h-[calc(100vh-130px)] 
-      flex-col justify-center 
-      bg-[url('/images/logo.png')] bg-cover bg-center bg-no-repeat max-sm:py-10"
-    >
-      <div className='container text-white backdrop-blur-md'>
-        <div className='text-center'>
-          <p className='text-6xl font-semibold'>Beyond Recruitment</p>
-          <p className='text-2xl font-semibold'>
-            Tech Talent & Executive Research
-          </p>
-        </div>
+    <>
+      <div className='h-fit bg-black'>
+        <motion.img
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.5, duration: 1 }}
+          src='/images/logo.jpg'
+          className='mx-auto'
+        />
+      </div>
+      <div className='container py-5'>
         <div className='mx-auto mt-5 max-w-2xl space-y-2'>
-          <p>
-            At Beyond Recruitment, we go beyond traditional recruitment
-            services, offering something innovative and exceptional.
-          </p>
-          {/* <p>
-            We specialize in delivering top-quality recruitment and talent
-            research services, tailored to your unique requirements. We believe
-            in building strong relationships with our clients, holding ourselves
-            accountable, and always striving for excellence.
-          </p> */}
-          <p>
-            Moreover, we understand the importance of cost-effectiveness,
-            ensuring that our services deliver great value without compromising
-            on quality.
-          </p>
-          <p>
-            You can count on Beyond Recruitment for exceptional service,
-            offering inventive approaches and unleashing the full potential of
-            your team
+          <p className='text-center text-xl'>
+            Trust Beyond Recruitment for innovative solutions that maximize your
+            team's potential. Specializing in top-quality recruitment and talent
+            research, tailored to your needs, we prioritize cost-effectiveness
+            without compromising quality.
           </p>
           <p className='mx-auto max-w-2xl pt-5 text-center text-2xl font-semibold'>
             Our mission is clear: to fulfill our clients' needs with precision
@@ -39,6 +26,6 @@ export default function MainSection() {
           </p>
         </div>
       </div>
-    </div>
+    </>
   );
 }
