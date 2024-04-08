540aec1a2fc4024454f03186e9b0bf6984c95634
saneag <garstea2002@mail.ru>
2024-04-08 13:24:58 +0300

    added linkedin page link in header

--- fe/src/components/Header/HeaderContact.tsx
+++ fe/src/components/Header/HeaderContact.tsx
@@ -3,7 +3,11 @@ import { Mail, Phone } from 'lucide-react';
 export default function HeaderContact() {
   return (
     <div className='container flex flex-wrap justify-end gap-x-5 gap-y-2 pt-2 text-sm'>
-      <a href='#' className='group flex items-center gap-2'>
+      <a
+        href='https://www.linkedin.com/company/102391126/admin/feed/posts/?feedType=following'
+        target='_blank'
+        className='group flex items-center gap-2'
+      >
         <img src='/images/linkedin.png' className='size-5' />
         <span>LinkedIn</span>
       </a>
